ebc35361b403920bba554e0834e8ea80743996b2
Shrey69 <agarwalshrey78@gmail.com>
2024-02-28 16:43:51 +0530

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-slate-900 h-16 flex justify-center items-center'>
+        
+      <div className='p-10 m-10 '>
+      <input
+      type='text'
+      className='w-[130%] h-9 pl-6 rounded-lg' 
+      />
+      </div>
+
+      <div>
+      {["Categories", "Website Builders", "Today's deal"].map((item, index)=> (
+            <a className={`text-md font-light text-white pl-12 ${index===2 && "ml-8"} `} key={index}>{item}</a>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Header
